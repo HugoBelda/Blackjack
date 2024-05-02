143641b72e9e77e680cefcb100bc8cbc8ff56402
Hugo Belda <hugobeldarevert@icloud.com>
2024-05-02 12:30:21 +0200

    Clase Deck.cs implementada + Puntuación de Player y Dealer
    Clase Deck.cs implementada + Puntuación de Player y Dealer en el canvas de la escena

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Deck : MonoBehaviour
 {
@@ -11,120 +12,184 @@ public class Deck : MonoBehaviour
     public Button playAgainButton;
     public Text finalMessage;
     public Text probMessage;
+    public Text TextPointsP;
+    public Text TextPointsD;
 
     public int[] values = new int[52];
-    int cardIndex = 0;    
-       
+    int cardIndex = 0;
+    
     private void Awake()
-    {    
-        InitCardValues();        
-
+    {
+        InitCardValues();
     }
-
     private void Start()
     {
         ShuffleCards();
-        StartGame();        
+        StartGame();
     }
-
     private void InitCardValues()
     {
-        /*TODO:
-         * Asignar un valor a cada una de las 52 cartas del atributo "values".
-         * En principio, la posición de cada valor se deberá corresponder con la posición de faces. 
-         * Por ejemplo, si en faces[1] hay un 2 de corazones, en values[1] debería haber un 2.
-         */
+        for (int i = 0; i < values.Length; i++)
+        {
+            int rango = i % 13; 
+        
+            if (rango == 0) 
+            {
+                values[i] = 1;
+            }
+            else if (rango >= 1 && rango <= 9) 
+            {
+                values[i] = rango + 1;
+            }
+            else 
+            {
+                values[i] = 10; 
+            }
+        }
     }
-
     private void ShuffleCards()
     {
-        /*TODO:
-         * Barajar las cartas aleatoriamente.
-         * El método Random.Range(0,n), devuelve un valor entre 0 y n-1
-         * Si lo necesitas, puedes definir nuevos arrays.
-         */       
-    }
+        for (int i = 51; i > 0; i--)
+        {
+            int j = UnityEngine.Random.Range(0, i + 1);
+            Sprite tempSprite = faces[i];
+            faces[i] = faces[j];
+            faces[j] = tempSprite;
 
+            int tempValue = values[i];
+            values[i] = values[j];
+            values[j] = tempValue;
+        }
+    }
     void StartGame()
     {
         for (int i = 0; i < 2; i++)
         {
             PushPlayer();
             PushDealer();
-            /*TODO:
-             * Si alguno de los dos obtiene Blackjack, termina el juego y mostramos mensaje
-             */
         }
     }
-
     private void CalculateProbabilities()
     {
-        /*TODO:
-         * Calcular las probabilidades de:
-         * - Teniendo la carta oculta, probabilidad de que el dealer tenga más puntuación que el jugador
-         * - Probabilidad de que el jugador obtenga entre un 17 y un 21 si pide una carta
-         * - Probabilidad de que el jugador obtenga más de 21 si pide una carta          
-         */
-    }
+        int playerPoints = player.GetComponent<CardHand>().points;
+        int[] restantes = new int[52 - cardIndex];
+        Array.Copy(values, cardIndex, restantes, 0, 52 - cardIndex);
 
+        int valorCartaOcultaDealer = values[cardIndex];
+        float probabilidadDealerGanaConCartaOculta = 0f;
+
+        for (int i = 0; i < restantes.Length; i++)
+        {
+            int puntosPosiblesDealer = valorCartaOcultaDealer + restantes[i];
+            if (puntosPosiblesDealer > playerPoints && puntosPosiblesDealer <= 21)
+            {
+                probabilidadDealerGanaConCartaOculta += 1f;
+            }
+        }
+        probabilidadDealerGanaConCartaOculta = (probabilidadDealerGanaConCartaOculta / restantes.Length) * 100f;
+
+        float probabilidadJugadorEntre17Y21 = 0f;
+        for (int i = 0; i < restantes.Length; i++)
+        {
+            int puntosPosiblesJugador = playerPoints + restantes[i];
+            if (puntosPosiblesJugador >= 17 && puntosPosiblesJugador <= 21)
+            {
+                probabilidadJugadorEntre17Y21 += 1f;
+            }
+        }
+        
+        probabilidadJugadorEntre17Y21 = (probabilidadJugadorEntre17Y21 / restantes.Length) * 100f;
+
+        float probabilidadJugadorSePasa = 0f;
+        for (int i = 0; i < restantes.Length; i++)
+        {
+            int puntosPosiblesJugador = playerPoints + restantes[i];
+            if (puntosPosiblesJugador > 21)
+            {
+                probabilidadJugadorSePasa += 1f;
+            }
+        }
+
+        probabilidadJugadorSePasa = (probabilidadJugadorSePasa / restantes.Length) * 100f;
+        probMessage.text =
+            $"Dealer > Jugador: {probabilidadDealerGanaConCartaOculta}%\n" +
+            $"17<=x<=21: {probabilidadJugadorEntre17Y21}%\n" +
+            $"x>21: {probabilidadJugadorSePasa}%";
+    }
     void PushDealer()
     {
-        /*TODO:
-         * Dependiendo de cómo se implemente ShuffleCards, es posible que haya que cambiar el índice.
-         */
-        dealer.GetComponent<CardHand>().Push(faces[cardIndex],values[cardIndex]);
-        cardIndex++;        
+        dealer.GetComponent<CardHand>().Push(faces[cardIndex], values[cardIndex]);
+        cardIndex++;
     }
 
     void PushPlayer()
     {
-        /*TODO:
-         * Dependiendo de cómo se implemente ShuffleCards, es posible que haya que cambiar el índice.
-         */
-        player.GetComponent<CardHand>().Push(faces[cardIndex], values[cardIndex]/*,cardCopy*/);
+        player.GetComponent<CardHand>().Push(faces[cardIndex], values[cardIndex]);
         cardIndex++;
+        int playerPoints = player.GetComponent<CardHand>().points;
+        TextPointsP.text = $" {playerPoints}";
+
         CalculateProbabilities();
-    }       
+    }
 
     public void Hit()
     {
-        /*TODO: 
-         * Si estamos en la mano inicial, debemos voltear la primera carta del dealer.
-         */
-        
-        //Repartimos carta al jugador
         PushPlayer();
-
-        /*TODO:
-         * Comprobamos si el jugador ya ha perdido y mostramos mensaje
-         */      
-
+        CalculateProbabilities();
+        int playerPoints = player.GetComponent<CardHand>().points;
+        if (playerPoints > 21)
+        {
+            finalMessage.text = "El jugador pierde!";
+            EndGame();
+        }
     }
 
     public void Stand()
     {
-        /*TODO: 
-         * Si estamos en la mano inicial, debemos voltear la primera carta del dealer.
-         */
+        dealer.GetComponent<CardHand>().InitialToggle();
+        int dealerPoints = dealer.GetComponent<CardHand>().points;
+        while (dealerPoints < 17)
+        {
+            PushDealer();
+            dealerPoints = dealer.GetComponent<CardHand>().points;
+        }
+        TextPointsD.text = $"{dealerPoints}";
+        int playerPoints = player.GetComponent<CardHand>().points;
+        dealerPoints = dealer.GetComponent<CardHand>().points;
 
-        /*TODO:
-         * Repartimos cartas al dealer si tiene 16 puntos o menos
-         * El dealer se planta al obtener 17 puntos o más
-         * Mostramos el mensaje del que ha ganado
-         */                
-         
+        if (dealerPoints > 21)
+        {
+            finalMessage.text = "El dealer pierde!";
+        }
+        else if (playerPoints > dealerPoints)
+        {
+            finalMessage.text = "El jugador gana!";
+        }
+        else if (playerPoints < dealerPoints)
+        {
+            finalMessage.text = "El dealer gana!";
+        }
+        else
+        {
+            finalMessage.text = "Empate!";
+        }
+        EndGame();
     }
-
+    
     public void PlayAgain()
     {
         hitButton.interactable = true;
         stickButton.interactable = true;
         finalMessage.text = "";
         player.GetComponent<CardHand>().Clear();
-        dealer.GetComponent<CardHand>().Clear();          
+        dealer.GetComponent<CardHand>().Clear();
         cardIndex = 0;
         ShuffleCards();
         StartGame();
     }
-    
+    private void EndGame()
+    {
+        hitButton.interactable = false;
+        stickButton.interactable = false;
+    }
 }
